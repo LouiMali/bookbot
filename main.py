1541d2d101825c8c38db825cad137aba0cc2f61b
github_dakotabammer <louismalinowski1993@gmail.com>
2025-06-02 12:30:22 +0200

    added functions read and count_words to main.py

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("greetings boots")
+def get_book_text(path_to_file):
+    file_contents = ""
+    
+    with open(path_to_file) as f:
+        file_contents = f.read()
+
+
+    return file_contents
+
+def word_count(file_contents):
+    words = file_contents.split()
+    counter = 0
+
+    for word in words:
+        counter += 1
+
+    return counter
+
+
+def main():
+    book_text = get_book_text("books/frankenstein.txt")
+    num_words = word_count(book_text)
+    print(f"{num_words} words found in the document")
+
+main()
